156c5ba0615cd3c6510ac9d41f98c8475d8775ac
Harir27 <harir27@gmail.com>
2024-10-06 18:48:53 +0800

    app.py

--- app.py
+++ app.py
@@ -1,20 +1,10 @@
-from flask import Flask,render_template,request
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/",methods=["GET","POST"])
-def index():
-    return(render_template("index.html"))
+@app.route('/')
+def home():
+    return jsonify({"message": "Hello, RoboProp Advisor Backend!"})
 
-@app.route("/prediction_DBS",methods=["GET","POST"])
-def prediction_DBS():
-    return(render_template("prediction_DBS.html"))
-
-@app.route("/prediction_result_DBS",methods=["GET","POST"])
-def prediction_result_DBS():
-    q = float(request.form.get("q"))
-    r = (-50.6 * q) + 90.2
-    return(render_template("prediction_result_DBS.html",r=r))
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
